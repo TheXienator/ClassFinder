b0a44a1861fe7823c6741a256c54295c0ee999cb
Jason Xie <jason.xie@berkeley.edu>
2015-09-04 16:55:20 -0400

    added project

---
+++ ClassFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassFinder
+//
+//  Created by Jason Xie on 9/4/15.
+//  Copyright (c) 2015 Jason Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClassFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassFinder
+//
+//  Created by Jason Xie on 9/4/15.
+//  Copyright (c) 2015 Jason Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
